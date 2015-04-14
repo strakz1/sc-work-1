f277b00de57c915c41c938c5298a193ec71a7f50
Zdenek Straka <zdenek.straka@etnetera.cz>
2015-04-14 11:13:27 +0200

    Init commit

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "sc-work-1"
+
+version := "1.0"
+
+scalaVersion := "2.11.6"
+    
---
+++ src/main/scala/cz/etn/scwork/HelloWorld.scala
@@ -0,0 +1,10 @@
+package cz.etn.scwork
+
+
+object HelloWorld {
+
+  def main(args: Array[String]) {
+    println("hello world")
+  }
+
+}
